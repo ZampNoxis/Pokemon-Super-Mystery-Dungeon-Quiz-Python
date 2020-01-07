9f07814e44dfea71ab9e5356e00573cf861682ae
ZampNoxis <sallen4@att.net>
2020-01-06 19:15:38 -0600

    Assigned each randomly picked question to a variable. Fixed indention issues causing code to end if 2 was selected.

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ def input_func():
 
 # Initialize all 5 values to False
 sunny = calm = flexible = serious = cooperate = False
-
-print(random.choice(questions.question1))
+# TODO: Turn inputs and if-else statements into a function with a while loop
+q1_var = random.choice(questions.question1)
+print(q1_var)
 
 # The two questions for question 1 are randomly picked. The corresponding answers are outputted depending on the
 # question picked.
-if random.choice(questions.question1) == questions.question1[0]:
+if q1_var == questions.question1[0]:
     print(questions.q1_answers[0])
 else:
     print(questions.q1_answers[1])
@@ -25,72 +26,66 @@ input_choice = input_func()
 if input_choice == 1 or input_choice == 2:
     if input_choice == 1:
         sunny = True
+
+    q2_var = random.choice(questions.question2)
+    print(q2_var)
+
+    if q2_var == questions.question2[0]:
+        print(questions.q2_answers[0])
     else:
-        pass
+        print(questions.q2_answers[1])
 
-        print(random.choice(questions.question2))
+    input_choice = input_func()
+    if input_choice == 1 or input_choice == 2:
+        if input_choice == 1:
+            calm = True
 
-        if random.choice(questions.question2) == questions.question2[0]:
-            print(questions.q2_answers[0])
-        else:
-            print(questions.q2_answers[1])
+            q3_var = random.choice(questions.question3)
+            print(q3_var)
 
-        input_choice = input_func()
-        if input_choice == 1 or input_choice == 2:
-            if input_choice == 1:
-                calm = True
+            if q3_var == questions.question3[0]:
+                print(questions.q3_answers[0])
             else:
-                pass
+                print(questions.q3_answers[1])
 
-                print(random.choice(questions.question3))
+            input_choice = input_func()
+            if input_choice == 1 or input_choice == 2:
+                if input_choice == 1:
+                    flexible = True
 
-                if random.choice(questions.question3) == questions.question3[0]:
-                    print(questions.q3_answers[0])
-                else:
-                    print(questions.q3_answers[1])
+                    q4_var = random.choice(questions.question4)
+                    print(q4_var)
 
-                input_choice = input_func()
-                if input_choice == 1 or input_choice == 2:
-                    if input_choice == 1:
-                        flexible = True
+                    if q4_var == questions.question4[0]:
+                        print(questions.q4_answers[0])
                     else:
-                        pass
+                        print(questions.q4_answers[1])
 
-                        print(random.choice(questions.question4))
+                    input_choice = input_func()
+                    if input_choice == 1 or input_choice == 2:
+                        if input_choice == 1:
+                            serious = True
 
-                        if random.choice(questions.question4) == questions.question4[0]:
-                            print(questions.q4_answers[0])
-                        else:
-                            print(questions.q4_answers[1])
+                            q5_var = random.choice(questions.question5)
+                            print(q5_var)
 
-                        input_choice = input_func()
-                        if input_choice == 1 or input_choice == 2:
-                            if input_choice == 1:
-                                serious = True
+                            if q5_var == questions.question5[0]:
+                                print(questions.q5_answers[0])
                             else:
-                                pass
+                                print(questions.q5_answers[1])
 
-                                print(random.choice(questions.question5))
-
-                                if random.choice(questions.question5) == questions.question5[0]:
-                                    print(questions.q5_answers[0])
-                                else:
-                                    print(questions.q5_answers[1])
-
-                                input_choice = input_func()
-                                if input_choice == 1 or input_choice == 2:
-                                    if input_choice == 1:
-                                        cooperate = True
-                                    else:
-                                        pass
-                                    # TODO: If-else statements for all 5 boolean values
-                                else:
-                                    print('Please enter either 1 or 2.')
-                        else:
-                            print('Please enter either 1 or 2.')
-                else:
-                    print('Please enter either 1 or 2.')
-        else:
-            print('Please enter either 1 or 2.')
+                            input_choice = input_func()
+                            if input_choice == 1 or input_choice == 2:
+                                if input_choice == 1:
+                                    cooperate = True
+                                # TODO: If-else statements for all 5 boolean values
+                            else:
+                                print('Please enter either 1 or 2.')
+                    else:
+                        print('Please enter either 1 or 2.')
+            else:
+                print('Please enter either 1 or 2.')
+    else:
+        print('Please enter either 1 or 2.')
 else:
     print('Please enter either 1 or 2.')
