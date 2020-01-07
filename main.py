70f975120cfac4fa4ffed313099bb9f12717866c
ZampNoxis <sallen4@att.net>
2020-01-06 19:25:49 -0600

    More indention fixes. Updated comments.

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ def input_func():
 
 # Initialize all 5 values to False
 sunny = calm = flexible = serious = cooperate = False
+
 # TODO: Turn inputs and if-else statements into a function with a while loop
+# One of the two questions for question 1 are randomly picked and assigned to a variable. The corresponding answers are
+# outputted depending on the question picked.
 q1_var = random.choice(questions.question1)
 print(q1_var)
 
-# The two questions for question 1 are randomly picked. The corresponding answers are outputted depending on the
-# question picked.
 if q1_var == questions.question1[0]:
     print(questions.q1_answers[0])
 else:
@@ -40,51 +41,52 @@ if input_choice == 1 or input_choice == 2:
         if input_choice == 1:
             calm = True
 
-            q3_var = random.choice(questions.question3)
-            print(q3_var)
+        q3_var = random.choice(questions.question3)
+        print(q3_var)
 
-            if q3_var == questions.question3[0]:
-                print(questions.q3_answers[0])
+        if q3_var == questions.question3[0]:
+            print(questions.q3_answers[0])
+        else:
+            print(questions.q3_answers[1])
+
+        input_choice = input_func()
+        if input_choice == 1 or input_choice == 2:
+            if input_choice == 1:
+                flexible = True
+
+            q4_var = random.choice(questions.question4)
+            print(q4_var)
+
+            if q4_var == questions.question4[0]:
+                print(questions.q4_answers[0])
             else:
-                print(questions.q3_answers[1])
+                print(questions.q4_answers[1])
 
             input_choice = input_func()
             if input_choice == 1 or input_choice == 2:
                 if input_choice == 1:
-                    flexible = True
+                    serious = True
 
-                    q4_var = random.choice(questions.question4)
-                    print(q4_var)
+                q5_var = random.choice(questions.question5)
+                print(q5_var)
 
-                    if q4_var == questions.question4[0]:
-                        print(questions.q4_answers[0])
-                    else:
-                        print(questions.q4_answers[1])
+                if q5_var == questions.question5[0]:
+                    print(questions.q5_answers[0])
+                else:
+                    print(questions.q5_answers[1])
 
-                    input_choice = input_func()
-                    if input_choice == 1 or input_choice == 2:
-                        if input_choice == 1:
-                            serious = True
-
-                            q5_var = random.choice(questions.question5)
-                            print(q5_var)
-
-                            if q5_var == questions.question5[0]:
-                                print(questions.q5_answers[0])
-                            else:
-                                print(questions.q5_answers[1])
-
-                            input_choice = input_func()
-                            if input_choice == 1 or input_choice == 2:
-                                if input_choice == 1:
-                                    cooperate = True
-                                # TODO: If-else statements for all 5 boolean values
-                            else:
-                                print('Please enter either 1 or 2.')
-                    else:
-                        print('Please enter either 1 or 2.')
+                input_choice = input_func()
+                if input_choice == 1 or input_choice == 2:
+                    if input_choice == 1:
+                        cooperate = True
+                    # TODO: If-else statements for all 5 boolean values
+                    print(sunny, calm, flexible, serious, cooperate)
+                else:
+                    print('Please enter either 1 or 2.')
             else:
                 print('Please enter either 1 or 2.')
+        else:
+            print('Please enter either 1 or 2.')
     else:
         print('Please enter either 1 or 2.')
 else:
