4350a52beebf13d61cfcb9fd91ee23a888086d4e
ZampNoxis <sallen4@att.net>
2020-01-11 20:07:43 -0600

    Added separation border and if-else statements with commented-out personality types and descriptions

--- main.py
+++ main.py
@@ -79,8 +79,174 @@ if input_choice == 1 or input_choice == 2:
                 if input_choice == 1 or input_choice == 2:
                     if input_choice == 1:
                         cooperate = True
-                    # TODO: If-else statements for all 5 boolean values
-                    print(sunny, calm, flexible, serious, cooperate)
+                    # TODO: Add logic to if-else statements
+                    for x in range(50):
+                        print('-', end='')
+
+                    if not sunny and not calm and not flexible and not serious:
+                        # Unmoving
+                        # You're a little shy and perhaps too concerned with others' well-being.
+                        #
+                        # But that's precisely why you're so kind to those around you.
+                        #
+                        # That warm, caring personality must be...
+                        pass
+                    elif not sunny and not calm and serious and not cooperate:
+                        # Man
+                        # Do people think you're a loner? Or that you have an unapproachable aura?
+                        #
+                        # The truth is that you're a decisive person who's very sure of yourself!
+                        #
+                        # That strong personality must be...
+                        pass
+                    elif not sunny and not calm and not flexible and serious and cooperate:
+                        # Docile
+                        # You get along with everyone. You're well liked for cherishing your friends.
+                        #
+                        # You're unambitious and calm, but you're actually a hard worker who everyone admires.
+                        #
+                        # As popular as you are, you must be...
+                        pass
+                    elif not sunny and not calm and flexible and not serious and not cooperate:
+                        # Man
+                        # You always trust your feelings, even if they get you in trouble sometimes.
+                        #
+                        # But clearly expressing your feelings is proof you want others to understand you.
+                        #
+                        # A straight shooter like you must be...
+                        pass
+                    elif not sunny and not calm and flexible and cooperate:
+                        # Unmoving
+                        # You may get upset a little easily and feel uneasy when you don't fit in.
+                        #
+                        # But it's your delicate heart that lets you better understand others' feelings.
+                        #
+                        # With that sensitive personality, you must be...
+                        pass
+                    elif not sunny and calm and not flexible and not cooperate:
+                        # Unmoving
+                        # You're sure of yourself and unflappable. You have all the qualities of a leader.
+                        #
+                        # Just by being around you, others feel more confident.
+                        #
+                        # That charismatic personality must belong to...
+                        pass
+                    elif not sunny and calm and not flexible and cooperate:
+                        # Unmoving
+                        # Some people work hard in the background. You're one of those, aren't you?
+                        #
+                        # When you're not around, things don't go well. Everyone can always count on you.
+                        #
+                        # Someone who pulls the strings in the background like that must be...
+                        pass
+                    elif not sunny and calm and flexible and not cooperate:
+                        # Docile
+                        # You love tackling new challenges with your calm, cool, collected reasoning.
+                        #
+                        # You're so smart, you know the answers before you've even heard the question.
+                        #
+                        # That calm, reliable personality must belong to...
+                        pass
+                    elif not sunny and calm and flexible and not serious and cooperate:
+                        # Comfortable
+                        # You know your place and don't overdo things, but you're really motivated.
+                        #
+                        # Everyone looks to you as an example. You may have many admirers...
+                        #
+                        # That serene, lovely personality must belong to...
+                        pass
+                    elif not sunny and calm and flexible and serious and cooperate:
+                        # Comfortable
+                        # You're never shaken and can always read between the lines. You never give up.
+                        #
+                        # You're quite likable, and you're always surrounded by friends.
+                        #
+                        # With a good-natured personality like that, you must be...
+                        pass
+                    elif sunny and not calm and not flexible and not serious:
+                        # Calm
+                        # You're very active, maybe too much so.
+                        # You may tire others out.
+                        #
+                        # But you are amazing.
+                        # You make the most of your time, after all!
+                        #
+                        # Being a wee bit impatient, you must be...
+                        pass
+                    elif sunny and not calm and not flexible and serious:
+                        # Calm
+                        # You might be a bit of a space cadet, but when it's time to work, you work hard.
+                        #
+                        # You inspire courage in others without even knowing it.
+                        #
+                        # With that hardworking personality, you must be...
+                        pass
+                    elif sunny and not calm and flexible and not serious:
+                        # Man
+                        # You're a fearless challenger at all times! But you're also quick to give up.
+                        #
+                        # It's important to try out many different things to see what's right for you, though.
+                        #
+                        # As playful as you are, you must be...
+                        pass
+                    elif sunny and not calm and flexible and serious and not cooperate:
+                        # Docile
+                        # You carry yourself with thunderous authority. You're brave enough to speak your mind.
+                        #
+                        # You can do anything well, so others rely on you.
+                        #
+                        # As brave as you are, you must be...
+                        pass
+                    elif sunny and not calm and flexible and serious and cooperate:
+                        # Docile
+                        # You're good natured and easygoing. But don't you feel nervous sometimes?
+                        #
+                        # That unique personality sets those around you at ease.
+                        #
+                        # As good natured as you are, you must be...
+                        pass
+                    elif sunny and calm and not flexible and not serious and not cooperate:
+                        # Man
+                        # You are you. Others are others. You stay true to what you want to do.
+                        #
+                        # Your actions make everyone feel all right. They all envy you.
+                        #
+                        # With that ability to make others happy, you must be...
+                        pass
+                    elif sunny and calm and not flexible and cooperate:
+                        # Comfortable
+                        # You're friendly and open minded. Aren't you already happy just as you are?
+                        #
+                        # You're so calm for your age... When you're happy, everyone is happy.
+                        #
+                        # As mild mannered as you are, you must be...
+                        pass
+                    elif sunny and calm and serious and not cooperate:
+                        # Calm
+                        # Everyone believes you're indecisive, but you actually just think before you act.
+                        #
+                        # Your positivity wins people over. It's no surprise you're so successful!
+                        #
+                        # With your ability to make everyone feel jolly, you must be...
+                        pass
+                    elif sunny and calm and flexible and not serious and not cooperate:
+                        # Calm
+                        # Everyone looks up to you. You're persuasive and get what you want.
+                        #
+                        # To those around you, your clever actions look really cool.
+                        #
+                        # As cool as you are, you must be...
+                        pass
+                    elif sunny and calm and flexible and cooperate:
+                        # Comfortable
+                        # Who's got time to worry? You're a genius at having fun!
+                        #
+                        # Your cheerfulness makes everyone smile. Every day is a happy day when you're around!
+                        #
+                        # With your ability to produce warm, fuzzy feelings all around you, you must be...
+                        pass
+                    else:
+                        print('Please enter either 1 or 2.')
                 else:
                     print('Please enter either 1 or 2.')
             else:
