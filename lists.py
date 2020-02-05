095967febf168df937c182b8042b0478f1dd04d6
ZampNoxis <sallen4@att.net>
2020-02-04 21:29:31 -0600

    Added results to list

--- lists.py
+++ lists.py
@@ -24,4 +24,63 @@ answers = [['\n1)Turn to them and introduce yourself.\n2)Wait nervously for them
 
 results = ["\nYou're a little shy and perhaps too concerned with others' well-being."
            "\nBut that's precisely why you're so kind to those around you."
-           "\nThat warm, caring personality must be..."]
+           "\nThat warm, caring personality must be...",
+           "\nDo people think you're a loner? Or that you have an unapproachable aura?"
+           "\nThe truth is that you're a decisive person who's very sure of yourself!"
+           "\nThat strong personality must be...",
+           "\nYou get along with everyone. You're well liked for cherishing your friends."
+           "\nYou're unambitious and calm, but you're actually a hard worker who everyone admires."
+           "\nAs popular as you are, you must be...",
+           "\nYou always trust your feelings, even if they get you in trouble sometimes."
+           "\nBut clearly expressing your feelings is proof you want others to understand you."
+           "\nA straight shooter like you must be...",
+           "\nYou may get upset a little easily and feel uneasy when you don't fit in."
+           "\nBut it's your delicate heart that lets you better understand others' feelings."
+           "\nWith that sensitive personality, you must be...",
+           "\nYou're sure of yourself and unflappable. You have all the qualities of a leader."
+           "\nJust by being around you, others feel more confident."
+           "\nThat charismatic personality must belong to...",
+           "\nSome people work hard in the background. You're one of those, aren't you?"
+           "\nWhen you're not around, things don't go well. Everyone can always count on you."
+           "\nSomeone who pulls the strings in the background like that must be...",
+           "\nYou love tackling new challenges with your calm, cool, collected reasoning."
+           "\nYou're so smart, you know the answers before you've even heard the question."
+           "\nThat calm, reliable personality must belong to...",
+           "\nYou know your place and don't overdo things, but you're really motivated."
+           "\nEveryone looks to you as an example. You may have many admirers..."
+           "\nThat serene, lovely personality must belong to...",
+           "\nYou're never shaken and can always read between the lines. You never give up."
+           "\nYou're quite likable, and you're always surrounded by friends."
+           "\nWith a good-natured personality like that, you must be...",
+           "\nYou're very active, maybe too much so."
+           "\nYou may tire others out."
+           "\nBut you are amazing."
+           "\nYou make the most of your time, after all!"
+           "\nBeing a wee bit impatient, you must be...",
+           "\nYou might be a bit of a space cadet, but when it's time to work, you work hard."
+           "\nYou inspire courage in others without even knowing it."
+           "\nWith that hardworking personality, you must be...",
+           "\nYou're a fearless challenger at all times! But you're also quick to give up."
+           "\nIt's important to try out many different things to see what's right for you, though."
+           "\nAs playful as you are, you must be...",
+           "\nYou carry yourself with thunderous authority. You're brave enough to speak your mind."
+           "\nYou can do anything well, so others rely on you."
+           "\nAs brave as you are, you must be...",
+           "\nou're good natured and easygoing. But don't you feel nervous sometimes?"
+           "\nThat unique personality sets those around you at ease."
+           "\nAs good natured as you are, you must be...",
+           "\nYou are you. Others are others. You stay true to what you want to do."
+           "\nYour actions make everyone feel all right. They all envy you."
+           "\nWith that ability to make others happy, you must be...",
+           "\nYou're friendly and open minded. Aren't you already happy just as you are?"
+           "\nYou're so calm for your age... When you're happy, everyone is happy."
+           "\nAs mild mannered as you are, you must be...",
+           "\nEveryone believes you're indecisive, but you actually just think before you act."
+           "\nYour positivity wins people over. It's no surprise you're so successful!"
+           "\nWith your ability to make everyone feel jolly, you must be...",
+           "\nEveryone looks up to you. You're persuasive and get what you want."
+           "\nTo those around you, your clever actions look really cool."
+           "\nAs cool as you are, you must be...",
+           "\nWho's got time to worry? You're a genius at having fun!"
+           "\nYour cheerfulness makes everyone smile. Every day is a happy day when you're around!"
+           "\nWith your ability to produce warm, fuzzy feelings all around you, you must be..."]
